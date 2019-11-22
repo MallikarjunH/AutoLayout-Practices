b6e6b042fb509d821979f33991d4fd75135e9f8f
Mallikarjun Hanagandi <mallikarjun.iphone9@gmail.com>
2019-11-22 17:39:11 +0530

    CotentHugging - Swift
    CotentHugging - Swift Basic project added

---
+++ Content_Hugging_Priority/Content_Hugging_Priority/Controllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Content_Hugging_Priority
+//
+//  Created by mallikarjun on 22/11/19.
+//  Copyright © 2019 Mallikarjun H. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
