b6d34958721804f5cc54178ecd96097790ae43f9
Mallikarjun Hanagandi <mallikarjun.iphone9@gmail.com>
2020-01-07 12:23:35 +0530

    AutoLayoutContraint Added
    AutoLayout Contraint Example by Yogesh Patel Added
    YoutTube Link: https://www.youtube.com/watch?v=kfl9xJRuVEk

---
+++ AutoLayoutConstraints_YP/AutoLayoutConstraints_YP/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AutoLayoutConstraints_YP
+//
+//  Created by mallikarjun on 07/01/20.
+//  Copyright © 2020 Mallikarjun H. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
