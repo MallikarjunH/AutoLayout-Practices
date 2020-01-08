e763dea254098766cadba73e742aaee296ff5fae
Mallikarjun Hanagandi <mallikarjun.iphone9@gmail.com>
2020-01-08 12:48:24 +0530

    ViewCotrollers Added

---
+++ YP_VideoSeries/Content_Hugging_Priority/Content_Hugging_Priority/Controllers/ContentHuggingPriority2.swift
@@ -0,0 +1,30 @@
+//
+//  ContentHuggingPriority2.swift
+//  Content_Hugging_Priority
+//
+//  Created by mallikarjun on 08/01/20.
+//  Copyright © 2020 Mallikarjun H. All rights reserved.
+//
+
+import UIKit
+
+class ContentHuggingPriority2: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ YP_VideoSeries/Content_Hugging_Priority/Content_Hugging_Priority/Controllers/ContentHuggingPriority3.swift
@@ -0,0 +1,30 @@
+//
+//  ContentHuggingPriority3.swift
+//  Content_Hugging_Priority
+//
+//  Created by mallikarjun on 08/01/20.
+//  Copyright © 2020 Mallikarjun H. All rights reserved.
+//
+
+import UIKit
+
+class ContentHuggingPriority3: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
