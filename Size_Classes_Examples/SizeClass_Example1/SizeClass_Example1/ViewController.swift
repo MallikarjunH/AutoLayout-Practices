86e194fa40b0e1e48c1c6433c92da4a7fb8a6587
Mallikarjun Hanagandi <mallikarjun.iphone9@gmail.com>
2020-01-16 16:16:12 +0530

    Example 1
    Example 1 - Size Class

---
+++ Size_Classes_Examples/SizeClass_Example1/SizeClass_Example1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SizeClass_Example1
+//
+//  Created by mallikarjun on 16/01/20.
+//  Copyright © 2020 Mallikarjun H. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    
+}
+
